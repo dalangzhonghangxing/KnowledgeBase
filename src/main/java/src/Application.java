0b47588b5e8e1c56f9b20166db4755beca02e5df
Hang <hang@HangdeMacBook-Pro.local>
2018-09-14 14:15:59 +0800

    修改pom

--- src/main/java/src/Application.java
+++ src/main/java/src/Application.java
@@ -1,3 +1,5 @@
+package src;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/src/controller/TestController.java
+++ src/main/java/src/controller/TestController.java
@@ -1,4 +1,4 @@
-package controller;
+package src.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
