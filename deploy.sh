69d032bd26a28548d51c2d17892bf43a6fb50d05
dalangzhonghangxing <553778439@qq.com>
2018-09-27 09:24:45 +0800

    完善自动化部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+# 部署后台
+cd KnowledgeBase
 PID=$(lsof -i:16666)
 echo "当前进程端口的进程号是 $PID"
 
@@ -13,4 +14,14 @@ git stash
 git pull -f origin master
 mvn clean
 mvn install
-nohup mvn spring-boot:run &
+nohup mvn spring-boot:run -Pprod &
+
+# 部署前台，下方目录需要根据实际情况进行修改
+cd /home/yjsyTest/tomcat/tomcat1/webapps/knowledge-base-ui
+git stash
+git pull -f origin master
+npm run-script clean
+npm run-script bower-install
+npm run-script dev
+cd build/common
+mv config_prob.js config.js
